9b83651aacefcc8ae4c801c37fc33701517edd71
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-15 14:56:10 -0600

    Completed Exercise #6

--- code.js
+++ code.js
@@ -7,15 +7,19 @@ function helloWorld() {
 
 //Exercise #1 - 5
 //sayHello function
-function sayHello(input){
-    if (input === 'Alex'){
-        return 'Hello, Alex!'
-    } else if (input === 'Pat'){
-        return 'Hello, Pat!'
-    }
-    return "Hello, Jane!";
-}
+// function sayHello(input){
+//     if (input === 'Alex'){
+//         return 'Hello, Alex!'
+//     } else if (input === 'Pat'){
+//         return 'Hello, Pat!'
+//     }
+//     return "Hello, Jane!";
+// }
 
+//Exercise #6 Refactor sayHello()
+function sayHello(input){
+    return "Hello, " + input + "!";
+}
 
 
 
